a80b2f7b2d94f12ccdfc1c9bf645c66e49b0f820
redaktorscha <nasonkina@yandex.ru>
2024-06-06 16:40:25 +0300

    fix deps, fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-   // Добавление ProviderScope позволяет использовать Riverpod для всего проекта
-   const ProviderScope(child: App()),
+  // Добавление ProviderScope позволяет использовать Riverpod для всего проекта
+  runApp(const ProviderScope(child: App()));
 }
 
 final countProvider = StateProvider<int>((ref) => 0);
@@ -13,26 +13,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<CounterModel>(
-          create: (context) => CounterModel(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter Demo Riverpod'),
         ),
-      ],
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Provider Counter Demo'),
-          ),
-          body: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IncrementButton(),
-                DecrementButton(),
-                CounterView(),
-              ],
-            ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IncrementButton(),
+              DecrementButton(),
+              CounterView(),
+            ],
           ),
         ),
       ),
@@ -51,7 +44,7 @@ class CounterView extends ConsumerWidget {
 
     return Text(
       '$count',
-      style: const TextStyle(color: Colors.green),
+      style: const TextStyle(color: Colors.purple),
     );
   }
 }
@@ -61,14 +54,13 @@ class IncrementButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // В этом случае нет необходимости прослушивать изменения, поэтому мы устанавливаем значение false в listen.
-    // var counter = Provider.of<CounterModel>(context, listen: false);
-    var counter = context.read<CounterModel>();
+    // В этом случае нет необходимости прослушивать изменения
+    final count = ref.read(countProvider.notifier);
 
     return IconButton(
       icon: const Icon(Icons.add, size: 12),
       onPressed: () {
-        counter.increment();
+        count.state++;
       },
     );
   }
@@ -80,14 +72,13 @@ class DecrementButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // В этом случае нет необходимости прослушивать изменения
-    // var counter = Provider.of<CounterModel>(context, listen: false);
-    final count = ref.read(countProvider);
+    final count = ref.read(countProvider.notifier);
 
     return IconButton(
       icon: const Icon(Icons.remove, size: 12),
       onPressed: () {
         // Это вызовет изменение состояния, в результате чего прослушивающие виджеты будут перестроены
-        count.state += 1;
+        count.state--;
       },
     );
   }
