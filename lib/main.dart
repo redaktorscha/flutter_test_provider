13d360a9fa03ad90134dd881fb78b101dcc24512
redaktorscha <nasonkina@yandex.ru>
2024-06-10 12:03:04 +0300

    fix title

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Provider demo',
+      title: 'Riverpod demo',
       home: HomeScreen(),
     );
   }
